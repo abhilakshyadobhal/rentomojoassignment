692cdac0418d031fcef5c786f9123c4e4dee4283
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-07-06 23:15:06 +0530

    herokuDB

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect("db4free.net:3306","abhisr4","Dobhal@7","abhimojo");
+$connect = mysqli_connect("db4free.net:3306","abhimojo","Dobhal@7","abhimojo");
 
 // Check connection
 if (mysqli_connect_errno())
