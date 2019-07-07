fcb9504f9121451fa289a5ae0b0e1594f9a9710f
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-07-07 23:27:46 +0530

    localhost added

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
 $connect = mysqli_connect("db4free.net:3306","abhimojo","Dobhal@7","abhimojo");
-
-// Check connection
+/*for local browser
+$connect = mysqli_connect("localhost","root","","rentomojo");
+*/// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
