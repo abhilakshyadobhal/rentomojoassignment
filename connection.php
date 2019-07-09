5c41dfe55e3e13f26dcb7c8e5a6deb4a0cc57468
abhilakshyadobhal <abhilakshyadobhal@gmail.com>
2019-07-09 23:48:39 +0530

    user login fixed

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-//$connect = mysqli_connect("db4free.net:3306","abhimojo","Dobhal@7","abhimojo");
-/*for local browser*/
+$connect = mysqli_connect("db4free.net:3306","abhimojo","Dobhal@7","abhimojo");
+/*for local browser
 $connect = mysqli_connect("localhost","root","","rentomojo");
-
+*/
 // Check connection
 if (mysqli_connect_errno())
   {
